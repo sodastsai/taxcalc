e53cdebf05f745a49d4bf4636ad9121b3e50fd32
Tien-Che Tsai <sodas@icloud.com>
2021-01-16 12:13:29 +0000

    Apply swiftlint

--- Tests/DataFormatTests/CurrencyTests.swift
+++ Tests/DataFormatTests/CurrencyTests.swift
@@ -4,16 +4,16 @@ import XCTest
 
 class CurrencyTests: XCTestCase {
   func testStringRepresentationOfPound() {
-    let gbp3_20 = Currency(amount: Decimal(string: "3.20")!, unit: .GBP)
-    XCTAssertEqual("\(gbp3_20)", "£3.20")
-    let gbp1024_32 = Currency(amount: Decimal(string: "-1024.32")!, unit: .GBP)
-    XCTAssertEqual("\(gbp1024_32)", "-£1,024.32")
+    let gbp1 = Currency(amount: Decimal(string: "3.20")!, unit: .GBP)
+    XCTAssertEqual("\(gbp1)", "£3.20")
+    let gbp2 = Currency(amount: Decimal(string: "-1024.32")!, unit: .GBP)
+    XCTAssertEqual("\(gbp2)", "-£1,024.32")
   }
 
   func testStringRepresentationOfDollar() {
-    let usd3_20 = Currency(amount: Decimal(string: "3.20")!, unit: .USD)
-    XCTAssertEqual("\(usd3_20)", "$3.20")
-    let usd1024_32 = Currency(amount: Decimal(string: "-1024.32")!, unit: .USD)
-    XCTAssertEqual("\(usd1024_32)", "-$1,024.32")
+    let usd1 = Currency(amount: Decimal(string: "3.20")!, unit: .USD)
+    XCTAssertEqual("\(usd1)", "$3.20")
+    let usd2 = Currency(amount: Decimal(string: "-1024.32")!, unit: .USD)
+    XCTAssertEqual("\(usd2)", "-$1,024.32")
   }
 }
